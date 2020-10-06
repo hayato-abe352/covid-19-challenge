d8125cc0706683c9d45c20139002a71bb96a9bd8
YukiShitara <yuki.shitara@basic.co.jp>
2020-10-06 16:07:32 +0900

    Agentを実装（動作未確認）

--- multi-agent-simulation/v2/Agent/Agent.py
+++ multi-agent-simulation/v2/Agent/Agent.py
@@ -1,13 +1,19 @@
 """
 エージェント定義
 """
+from __future__ import annotations
+
 import random
+from typing import List
 
 from Agent.Status import Status
+from Simulator.InfectionModel import InfectionModel
 
 
 class Agent:
-    def __init__(self, id, hometown, status: Status, infection_model):
+    def __init__(
+        self, id, hometown, status: Status, infection_model: InfectionModel
+    ):
         # 個体識別番号
         self.id = id
 
@@ -22,10 +28,47 @@ class Agent:
         # 感染症モデル
         self.infection_model = infection_model
 
-    def decide_next_status(self):
+        # 潜伏日数（発症までの残り日数）
+        self.incubation_count = 0
+
+    def decide_next_status(self, neighbors: List[Agent]):
         """ エージェントの次ステータスを決定 """
-        pass
+        # エージェントの状態変化ルール
+        #  [現在の状態]  [状態変化ルール]
+        #  SUSCEPTABLE  neighbors に含まれる EXPOSED / INFECTED の人数に応じて EXPOSED に推移
+        #  EXPOSED      一定時間経過後 INFECTED に推移
+        #  INFECTED     一定確率で RECOVERED に推移
+        #  RECOVERED    RECOVERED のまま変化なし
+
+        self.next_status = self.status
+
+        # SUSCEPTABLE
+        if self.status == Status.SUSCEPTABLE:
+            infecteds = [
+                agent
+                for agent in neighbors
+                if agent.status in [Status.EXPOSED, Status.INFECTED]
+            ]
+            prob = 1 - (
+                (1 - self.infection_model.infection_prob) ** len(infecteds)
+            )
+            if random.random() <= prob:
+                self.next_status = Status.EXPOSED
+                self.incubation_count = self.infection_model.incubation_period
+
+        # EXPOSED
+        if self.status == Status.EXPOSED:
+            count = self.incubation_count - 1
+            if count == 0:
+                self.next_status = Status.INFECTED
+            self.incubation_count = count
+
+        # INFECTED
+        if self.status == Status.INFECTED:
+            if random.random() <= self.infection_model.recovery_prob:
+                self.next_status = Status.RECOVERED
 
     def update_status(self):
         """ エージェントのステータスを更新 """
-        pass
+        self.status = self.next_status
+        self.next_status = None
